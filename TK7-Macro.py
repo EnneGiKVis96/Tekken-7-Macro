35ac25eda8a1d420da8a2f694319c10014b3748f
EnneGiKVis96 <93038396+EnneGiKVis96@users.noreply.github.com>
2022-06-13 20:15:57 +0200

    Update TK7-Macro.py
    added profile choice and more hotkeys

--- TK7-Macro.py
+++ TK7-Macro.py
@@ -2,9 +2,11 @@ from pynput import keyboard
 from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
 import time
 import os
+import threading
 
 forward = 'd'
 back = 'a'
+profile = "generic"
 
 #class availables
 class bcolors:
@@ -48,6 +50,90 @@ def WaveDash():
     time.sleep(0.02)
     releaseKey(key=forward)
 
+def OTGF1():
+    pressKey(key="NUM4")
+    time.sleep(0.02)
+    releaseKey(key="NUM4")
+    press(key="NUMLOCK")
+
+def PEWGF1():
+    pressKey(key=forward)
+    time.sleep(0.02)
+    releaseKey(key=forward)
+    time.sleep(0.02)
+
+def EWGF1():
+    pressKey(key=forward)
+    time.sleep(0.02)
+    releaseKey(key=forward)
+    time.sleep(0.05)
+    pressKey(key="s")
+    time.sleep(0.1)
+    releaseKey(key="s")
+
+def EWGF2():
+    pressKey(key="s")
+    time.sleep(0.02)
+    releaseKey(key="s")
+
+def EWGF3():
+    pressKey(key=forward)
+    time.sleep(0.02)
+    releaseKey(key=forward)
+
+def EWGF4():
+    pressKey(key="NUM8")
+    time.sleep(0.02)
+    releaseKey(key="NUM8")
+    press(key="NUMLOCK")
+
+def iWR2():
+    pressKey(key=forward)
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    time.sleep(0.002)
+    pressKey(key=forward)
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    time.sleep(0.002)
+    pressKey(key=forward)
+    time.sleep(0.002)
+    pressKey(key="NUM8")
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    releaseKey(key="NUM8")
+    time.sleep(0.002)
+    press(key="NUMLOCK")
+
+def iShiningWizard1():
+    pressKey(key=forward)
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    time.sleep(0.002)
+    pressKey(key=forward)
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    time.sleep(0.002)
+    pressKey(key=forward)
+    time.sleep(0.002)
+    x = threading.Thread(target=iShiningWizard2)
+    y = threading.Thread(target=iShiningWizard3)
+    x.start()
+    y.start()
+    time.sleep(0.002)
+    releaseKey(key=forward)
+    time.sleep(0.002)
+    press(key="NUMLOCK")
+
+def iShiningWizard2():
+    pressKey(key="NUM8")
+    time.sleep(0.002)
+    releaseKey(key="NUM8")
+
+def iShiningWizard3():
+    pressKey(key="NUM6")
+    time.sleep(0.002)
+    releaseKey(key="NUM6")
 
 def on_press(key):
     global forward
@@ -64,7 +150,6 @@ def on_press(key):
             WaveDash()
 
         elif key == keyboard.KeyCode(char = 'f'):
-            
             if forward == 'd':
                 forward = 'a'
                 back = 'd'
@@ -72,32 +157,153 @@ def on_press(key):
                 forward = 'd'
                 back = 'a'
 
+        elif profile == 'kazuya':
+            if key == keyboard.KeyCode(char = '7'):
+                x = threading.Thread(target=PEWGF1)
+                y = threading.Thread(target=EWGF2)
+                z = threading.Thread(target=EWGF3)
+                u = threading.Thread(target=EWGF4)
+                x.start()
+                time.sleep(0.05)
+                y.start()
+                z.start()
+                u.start()
+
+            elif key == keyboard.KeyCode(char = '9'):
+                x = threading.Thread(target=EWGF1)
+                y = threading.Thread(target=EWGF2)
+                z = threading.Thread(target=EWGF3)
+                u = threading.Thread(target=EWGF4)
+                x.start()
+                time.sleep(0.12)
+                y.start()
+                z.start()
+                u.start()
+
+        elif profile == 'heihachi':
+            if key == keyboard.KeyCode(char = '7'):
+                x = threading.Thread(target=PEWGF1)
+                y = threading.Thread(target=EWGF2)
+                z = threading.Thread(target=EWGF3)
+                u = threading.Thread(target=OTGF1)
+                x.start()
+                time.sleep(0.05)
+                y.start()
+                z.start()
+                u.start()
+            
+            elif key == keyboard.KeyCode(char = '9'):
+                x = threading.Thread(target=EWGF1)
+                y = threading.Thread(target=EWGF2)
+                z = threading.Thread(target=EWGF3)
+                u = threading.Thread(target=EWGF4)
+                x.start()
+                time.sleep(0.12)
+                y.start()
+                z.start()
+                u.start()
+
+        elif profile == 'jin':
+            if key == keyboard.KeyCode(char = '9'):
+                x = threading.Thread(target=EWGF1)
+                y = threading.Thread(target=EWGF2)
+                z = threading.Thread(target=EWGF3)
+                u = threading.Thread(target=EWGF4)
+                x.start()
+                time.sleep(0.12)
+                y.start()
+                z.start()
+                u.start()
+
+        elif profile == 'dragunov':
+            if key == keyboard.KeyCode(char = '9'):
+                iWR2()
+
+        elif profile == 'king':
+            if key == keyboard.KeyCode(char = '9'):
+                iShiningWizard1()
+
     except AttributeError:
         pass
 
 def on_release(key):
     pass
 
-clear_console()
-print("")
-print("")
-print("████████ ███████ ██   ██ ██   ██ ███████ ███    ██     ███████     ███    ███  █████   ██████ ██████   ██████  ")
-print("   ██    ██      ██  ██  ██  ██  ██      ████   ██          ██     ████  ████ ██   ██ ██      ██   ██ ██    ██ ")
-print("   ██    █████   █████   █████   █████   ██ ██  ██         ██      ██ ████ ██ ███████ ██      ██████  ██    ██ ")
-print("   ██    ██      ██  ██  ██  ██  ██      ██  ██ ██        ██       ██  ██  ██ ██   ██ ██      ██   ██ ██    ██ ")
-print("   ██    ███████ ██   ██ ██   ██ ███████ ██   ████        ██       ██      ██ ██   ██  ██████ ██   ██  ██████  ")
-print("                                                                                                               ")
-print("")
-print("[Q] for Backdash")
-print("[E] for Wavedash")
-print("[F] for P1/P2 side")
-print("[ESC] for quitting")
-print("")
-print(bcolors.WARNING + "Macro is running..." + bcolors.ENDC)
+def appTitle():
+    print(bcolors.OKGREEN + "")
+    print("")
+    print("████████ ███████ ██   ██ ██   ██ ███████ ███    ██     ███████     ███    ███  █████   ██████ ██████   ██████  ")
+    print("   ██    ██      ██  ██  ██  ██  ██      ████   ██          ██     ████  ████ ██   ██ ██      ██   ██ ██    ██ ")
+    print("   ██    █████   █████   █████   █████   ██ ██  ██         ██      ██ ████ ██ ███████ ██      ██████  ██    ██ ")
+    print("   ██    ██      ██  ██  ██  ██  ██      ██  ██ ██        ██       ██  ██  ██ ██   ██ ██      ██   ██ ██    ██ ")
+    print("   ██    ███████ ██   ██ ██   ██ ███████ ██   ████        ██       ██      ██ ██   ██  ██████ ██   ██  ██████  ")
+    print("                                                                                                               ")
+    print("")
 
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
 
+def menuStart():
+
+    global profile
+    clear_console()
+    appTitle()
+
+    print("[1] Kazuya")
+    print("[2] Heihachi")
+    print("[3] Jin/DevilJin")
+    print("[4] Claudio/Dragunov")
+    print("[5] King/Armor King")
+    print("[6] Generic")
+    print("[7] Exit")
+    print("")
+    choice = input(bcolors.WARNING + "Input the number near your choice -> " + bcolors.OKGREEN)
+
+    clear_console()
+    appTitle()
+    print(bcolors.WARNING + "--- [HOTKEY] ---" + bcolors.OKGREEN)
+    print("")
+
+    if choice == "1": 
+        profile = 'kazuya'
+        print("[NUM7] for PEWGF")
+        print("[NUM9] for EWGF")
+
+    elif choice == "2": 
+        profile = 'heihachi'
+        print("[NUM7] for OTGF")
+        print("[NUM9] for EWGF")
+
+    elif choice == "3": 
+        profile = 'jin'
+        print("[NUM9] for EWGF")
+
+    elif choice == "4": 
+        profile = 'dragunov'
+        print("[NUM9] for iWR2")
+
+    elif choice == "5": 
+        profile = 'king'
+        print("[NUM9] for iShiningWizard")
+
+    elif choice == "6": 
+        profile = 'generic'
+
+    elif choice == "7": 
+        exit()
+    else: 
+        exit()
+
+    print("[Q] for Backdash")
+    print("[E] for Wavedash")
+    print("[F] for P1/P2 side")
+    print("[ESC] for quitting")
+    print("")
+    print(bcolors.WARNING + "Macro is running..." + bcolors.ENDC)
+
+    # Collect events until released
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+
+menuStart()
